08d8001c3a8347f1883ce249f5da4499e8da0f8e
Eugenia Dellapenna <eugenia.dellapenna@gmail.com>
2013-07-06 10:01:55 -0700

    Empty xcode project

---
+++ App/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Well
+//
+//  Created by Eugenia Dellapenna on 7/6/13.
+//  Copyright (c) 2013 quavmo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
