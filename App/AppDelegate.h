e04de8126a904740fda8eef281e08183e16be892
Eugenia Dellapenna <eugenia.dellapenna@gmail.com>
2013-07-06 10:10:13 -0700

    git ignore

--- App/AppDelegate.h
+++ App/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  Well
-//
-//  Created by Eugenia Dellapenna on 7/6/13.
-//  Copyright (c) 2013 quavmo. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
--- App/AppDelegate.m
+++ App/AppDelegate.m
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.m
-//  Well
-//
-//  Created by Eugenia Dellapenna on 7/6/13.
-//  Copyright (c) 2013 quavmo. All rights reserved.
-//
-
 #import "AppDelegate.h"
 
 @implementation AppDelegate
@@ -13,7 +5,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
